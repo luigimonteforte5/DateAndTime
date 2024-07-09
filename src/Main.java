97f4e6c0d7c2a0eb72d845e7b5f99ac38549a486
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-07-09 18:19:19 +0200

    Replace zonedDateTime w/ OffsetDateTime

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
-import java.time.ZonedDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Main {
 	public static void main( String[] args ) {
 
-		/*
-		Crea un nuovo oggetto di tipo ZonedDateTime per assegnare uno ZoneId ed evitare l'eccezione
-		DateTimeException durante la formattazione
-		 */
-		ZonedDateTime data = ZonedDateTime.parse("2002-03-01T13:00:00Z");
+		OffsetDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z");
 
-		String fullData = data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
-		String medData = data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
-		String shortData = data.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+		String fullData = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+		String medData = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+		String shortData = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 
 		System.out.println("Data formattata in full: " + fullData);
 		System.out.println("Data formattata in medium: " + medData);
